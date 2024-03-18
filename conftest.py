703aec8ee16fab58adcf4025ce3760526195a86d
ilsinyakov <ilsinyakov@gmail.com>
2024-03-18 21:34:49 +0300

    Edit conftest

--- conftest.py
+++ conftest.py
@@ -7,7 +7,8 @@ def pytest_addoption(parser):
     parser.addoption('--browser_name', action='store', default='chrome',
                      help="Choose browser: chrome or firefox")
     parser.addoption('--language', action='store', default='en',
-                 help="Choose language: es, ru or en")
+                     help="Choose language: es, ru or en")
+
 
 @pytest.fixture(scope="function")
 def browser(request):            
