4011365fdae6b6a1ff870a22a2b9b02d0259ba20
ilsinyakov <ilsinyakov@gmail.com>
2024-03-18 21:28:46 +0300

    Add __init__

--- test_registration_page.py
+++ test_registration_page.py
@@ -1,6 +1,7 @@
 from .pages.base_page import BasePage
 from .pages.login_page import LoginPage
 from .pages.registration_page import RegistrationPage
+from time import sleep
 
 
 def test_guest_can_register(browser):
@@ -16,4 +17,4 @@ def test_guest_can_register(browser):
     login_page.go_to_registration_page()
     registration_page = RegistrationPage(browser, browser.current_url)
     registration_page.register_new_user(first_name, last_name, email, password)
-    
+    sleep(5)
